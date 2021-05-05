8a09659b43ab5a15a164d4f9cffafc0c0614dcde
Suraj Das <blogwapz@gmail.com>
2021-05-05 14:20:16 +0530

    Add connect.php page to connect with database.

---
+++ connect.php
@@ -0,0 +1,16 @@
+<?php
+        //Create Database Connection
+        $db_host="localhost";
+        $db_user="root";
+        $db_password="";
+        $db_name="imagex";
+        $connect= new mysqli($db_host,$db_user,$db_password,$db_name);
+
+        //Check Database Connection
+        if ($connect->connect_error) {
+            die ("Failed to connect!");
+        }
+        else{
+            echo "<font color='green'>Connected!</font><hr>";
+        }
+?>
