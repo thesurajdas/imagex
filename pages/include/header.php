1f5e98666b63a7979bbfb3f246b9820debdfe83c
Suraj Das <blogwapz@gmail.com>
2021-05-05 21:51:46 +0530

    Add Blank Page

--- pages/include/header.php
+++ pages/include/header.php
@@ -1,6 +1,18 @@
-<?php
-    echo '<header>
-    <nav class="navbar shadow-lg p-3 mb-5 bg-white rounded fixed-top navbar-expand-lg navbar-light bg-light">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
+        <link href="css/home.css" rel="stylesheet">
+
+            <!-------bootstrap css custom styling -> (OVERRIDE) <- --------------------->
+        <style>
+            .dropleft .dropdown-toggle::before{
+                border-right: 0;
+            }
+        </style>
+
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css">
+        
+<nav class="navbar shadow-lg p-3 mb-5 bg-white rounded fixed-top navbar-expand-lg navbar-light bg-light">
         <a class="navbar-brand" href="index.html">Gallery</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -57,5 +69,3 @@
             </ul>
         </div>
     </nav>
-</header>';
-?>
