f8efa95abf04fa7816b6955232f8b6f59d124614
Suraj Das <blogwapz@gmail.com>
2021-05-09 17:09:31 +0530

    Add logout page

--- pages/profile.php
+++ pages/profile.php
@@ -69,7 +69,7 @@
                                 <a class="dropdown-item" href="favimg.html">Saved Images</a>
                                 <a class="dropdown-item" href="usruploadimg.html">Your Uploads</a>
                                 <div class="dropdown-divider"></div>
-                                <a class="dropdown-item" href="#">SignOff</a>
+                                <a class="dropdown-item" href="/logout.php">Sign Out</a>
                             </div>
                         </li>
                         <li class="nav-item dropleft text-decoration-none">
