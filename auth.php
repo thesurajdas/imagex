5db668bba7ece249bd14be132c9f784d0a0719ba
Suraj Das <blogwapz@gmail.com>
2021-05-14 14:49:25 +0530

    fix last active users

--- auth.php
+++ auth.php
@@ -7,7 +7,7 @@ if(isset($_COOKIE['user_id'])){
     $user_id=$_COOKIE['user_id'];
     $user_id=convert_uudecode($user_id);
     //Get Data from SQL
-    $sql="SELECT * FROM users WHERE id='".$user_id."'";
+    $sql="SELECT * FROM users WHERE id='".$user_id."';";
     $result=$connect->query($sql);
     $row=$result->fetch_assoc();
     //Check cookie id with database id with === operator
@@ -16,7 +16,10 @@ if(isset($_COOKIE['user_id'])){
         $user_username=$row['username'];
         $user_name=$row['name'];
         $user_email=$row['email'];
+        //Add last active date
         $last_active=date("Y-m-d");
+        $sql2="UPDATE users SET last_active='$last_active' WHERE id='$id';";
+        $connect->query($sql2);
     }
     else{
         header("Location:login.php");
