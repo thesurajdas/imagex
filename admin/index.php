5dbee1c5023bca1849d75732f771142f0373d46f
Akash Sarkar <akash.sarkar1489@outlook.com>
2021-05-21 14:59:38 +0530

    commit change footer

--- admin/index.php
+++ admin/index.php
@@ -88,7 +88,7 @@ $total=$total->num_rows;
     <!-- partial:partials/_navbar.php -->
     <nav class="navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
       <div class="text-center navbar-brand-wrapper d-flex align-items-center justify-content-center">
-        <a class="navbar-brand brand-logo mr-5" href="../../index.php"><img src="../../images/logo.svg" class="mr-2" alt="logo"/></a>
+        <a class="navbar-brand brand-logo mr-5" href="../../index.php"><img src="images/logo.svg" class="mr-2" alt="logo"/></a>
         <a class="navbar-brand brand-logo-mini" href="../../index.php"><img src="../../images/logo-mini.svg" alt="logo"/></a>
       </div>
       <div class="navbar-menu-wrapper d-flex align-items-center justify-content-end">
--- index.php
+++ index.php
@@ -338,75 +338,67 @@
         </div>
         <!----------------------Footer Section---------------------------------------------------->
         <footer>
-                <div class="bg-light text-dark pt-5 pt-4">
-                    <div class="container">
-                        <div class="row text-center text-md-left ">
-                            <div class="col-md-3 col-lg-3 col-lx-3 mx-auto mt-3">
+            <div class="bg-light text-dark pt-1">
+                <div class="container">
+                    <div class="row text-center text-md-left ">
+                        <div class="col-md-10 col-xl-10 col-lg-10">
+                            <div class="col-12 mt-3">
                                 <h5 class="text-uppercase mb-4 font-wight-bold text-info text-dark">About Us</h5>
                                 <hrc class="mb-4">
-                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro ipsum ipsam quibusdam molestias, vero perspiciatis ea cum sapiente in nesciunt tempore blanditiis beatae corrupti et suscipit commodi animi iste molestiae!</p>
+                                <p>All started with a vision of giving all users a place where users upload and download their pictures taken by mobile phone. And we created <b>PIXWAVE</b>. <a style="text-decoration: none;" href="#"><i>READ MORE</i></a></p>
                             </div>
-                            <div class="col-md-2 col-lg-2 col-lx-2 mx-auto mt-3">
-                                <h5 class="text-uppercase mb-4 font-wight-bold text-info text-dark">Quick Links</h5>
-                                <hrc class="mb-4">
-                                    <p>
-                                        <a href="#" class="text-dark" style="text-decoration: none;" >Your Account</a>
-                                    </p>
-                                    <p>
-                                        <a href="#" class="text-dark" style="text-decoration: none;" >Favorites</a>
-                                    </p>
-                                    <p>
-                                        <a href="#" class="text-dark" style="text-decoration: none;" >Uploads</a>
-                                    </p>
-                                    <p>
-                                        <a href="#" class="text-dark" style="text-decoration: none;" >Trending</a>
-                                    </p>
-                            </div>
-                            <div class="col-md-4 col-lg-3 col-lx-3 mx-auto mt-3">
-                                <h5 class="text-uppercase mb-4 font-wight-bold text-info text-dark">Connect With Us</h5>
-                                <hrc class="mb-4">
-                                <p>
-                                    <i class="fas fa-house-user"></i> <span>Short Address,pin-0000000</span>
-                                        
-                                </p>
-                                <p>
-                                    <i class="fas fa-at"></i> <span>abc@xyz.com</span>
-                                        
-                                </p>
-                                <p>
-                                    <i class="fas fa-phone-square-alt"></i> <span>1234567890</span>
-                                        
-                                </p>
+                            <div class="col-12 mt-3">
+                                <div class="qll">
+                                    <h5 class="text-uppercase mb-4 font-wight-bold text-info text-dark">Quick Links</h5>
+                                    <div class="row">
+                                        <div class="col-md-3">
+                                            <p>
+                                                <a href="#" class="text-dark" style="text-decoration: none;" ><i class="fas fa-user-alt"></i> Account</a>
+                                            </p>
+                                        </div>
+                                        <div class="col-md-3">
+                                            <p>
+                                                <a href="#" class="text-dark" style="text-decoration: none;" ><i class="fab fa-gratipay"></i> Favorites</a>
+                                            </p>
+                                        </div>
+                                        <div class="col-md-3">
+                                            <p>
+                                                <a href="#" class="text-dark" style="text-decoration: none;" ><i class="fas fa-cloud-upload-alt"></i> Uploads</a>
+                                            </p>
+                                        </div>
+                                        <div class="col-md-3">
+                                            <p>
+                                                <a href="#" class="text-dark" style="text-decoration: none;" ><i class="fas fa-star-of-david"></i> Trending</a>
+                                            </p>
+                                        </div>
+                                    </div>    
+                                </div> 
                             </div>
                         </div>
-                        <hr class="mb-4">
-                        <div class="row d-flex justify-content-center">
-                            <div>
-                                <p>
-                                    Copyright &copy; <script>document.write(new Date().getFullYear())</script> <a href="/"><strong class="text-dark" style="text-decoration: none;"> Gallery Name </strong></a> - All rights reserved
-                                </p>
-                            </div>
+                        <div class="col-md-2 col-lg-2 col-xl-2 mt-3">
+                            <h5 class="text-uppercase mb-4 font-wight-bold text-info text-dark">Join Us</h5>
+                                <div class="container pl-0">
+                                    <div class="col-md-2">
+                                        <div class="social-icons">
+                                            <a href="#"><img src="pages/assets/img/fb.png"></a>
+                                            <a href="#"><img src="pages/assets/img/ins.png"></a>
+                                            <a href="#"><img src="pages/assets/img/tw.png"></a>
+                                            <a href="#"><img src="pages/assets/img/in.png"></a>
+                                        </div>
+                                    </div>
+                                </div>
                         </div>
-                        <!--<div class="row d-flex justify-content-center">
-                            <div class="text-center ">
-                                <ul class="list-unstyled list-inline">
-                                    <li class="list-inline-item">
-                                        <a href="#" class="text-dark" ><i class="bi bi-facebook"></i></a>
-                                    </li>
-                                    <li class="list-inline-item">
-                                        <a href="#" class= "text-dark" ><i class="bi bi-twitter"></i></a>
-                                    </li>
-                                    <li class="list-inline-item">
-                                        <a href="#" class=" text-dark" ><i class="fab bi bi-google"></i></a>
-                                    </li>
-                                    <li class="list-inline-item">
-                                        <a href="#" class=" text-dark" ><i class="bi bi-linkedin"></i></a>
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>-->
                     </div>
-                <div>    
+                    <hr class="mb-4">
+                    <div class="row d-flex justify-content-center">
+                        <div>
+                            <p>
+                                Copyright &copy; <script>document.write(new Date().getFullYear())</script> <a style="text-decoration: none;" href="/"><strong class="text-dark" style="text-decoration: none;"> PIXWAVE </strong></a> - All rights reserved
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            <div>    
         </footer>
     </body>
 </html>
