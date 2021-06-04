3e636849b1df67460b3fe4c790ab86a84a41b83d
Suraj Das <blogwapz@gmail.com>
2021-06-04 16:22:09 +0530

    fix edit profile button visibility

--- pages/profile.php
+++ pages/profile.php
@@ -278,7 +278,9 @@
                                                     <div class="row">
                                                         <h3 class="col-10 inm"><a class="card-link il" href="<?php echo $site_url; ?>/pages/image.php?id=<?php echo $row['image_id']; ?>"><?php echo $row['title']; ?></a></h3>
                                                         <div class="btn-group dropleft col-2">
+                                                        <?php if(($login==1) && ($user_id==$id)){ ?>
                                                             <button type="button" class="btn text-white dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fas fa-ellipsis-v"></i></button>
+                                                        <?php } ?>
                                                             <div class="dropdown-menu">
                                                                 <a class="dropdown-item" href="#">
                                                                     <button type="button" class="btn col-12" data-toggle="modal" data-target="#staticBackdrop"><i class="fad fa-file-edit"></i> Edit</button>
