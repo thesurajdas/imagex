460648a23ac2180d428d315df595b6840bc45b08
Suraj Das <blogwapz@gmail.com>
2021-06-08 21:44:05 +0530

    fix page load in trending page

--- pages/trending.php
+++ pages/trending.php
@@ -31,85 +31,11 @@
     <!-----------------------------------------------image-section-------------------------------------------->
 
     <div class="container shadow-lg p-3 mb-5 bg-white glry" style="border-radius: 1.25rem;">
-        <div class="col-md-12 pb-3 pt-2"><h1 class="text-center chdr" style="color:crimson"><i class="fad fa-fire"></i> Trendings</h1></div>
-        <div class="container shadow-lg p-3" style="border-radius: 1.25rem;">
-            <div class="row">
-                        <!--User Upoaded image start-->
-                        <?php
-	                            //Get Image Data from Database
-	                            $sql="SELECT * FROM images";
-	                            $result_img=$connect->query($sql);
-	                            if ($result_img->num_rows>0) {
-                                while($row=$result_img->fetch_assoc()): ?>
-                                    <div class="col-lg-4 col-md-6 col-sm-12 sglry">
-                                        <div class="card cds">
-                                            <img class="im" src="<?php echo $site_url,$row['image_location']; ?>" alt="Card image cap">
-                                            <div class="card-text cds-txt">
-                                                <div class="container" style="padding-left: 0">
-                                                    <div class="row">
-                                                        <h3 class="col-10 inm"><a class="card-link il" href="<?php echo $site_url; ?>/pages/image.php?id=<?php echo $row['image_id']; ?>"><?php echo $row['title']; ?></a></h3>
-                                                        <div class="btn-group dropleft col-2">
-                                                        <?php if(($login==1) && ($user_id==$row['user_id'])){ ?>
-                                                            <button type="button" class="btn text-white dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fad fa-ellipsis-v"></i></button>
-                                                        <?php } ?>
-                                                            <div class="dropdown-menu">
-                                                                <a class="dropdown-item" href="#">
-                                                                    <button type="button" class="btn col-12" data-toggle="modal" data-target="#staticBackdrop"><i class="fad fa-file-edit"></i> Edit</button>
-                                                                </a>
-                                                                <div class="dropdown-divider"></div>
-                                                                <a class="dropdown-item" href="<?php echo $site_url,$row['image_location']; ?>" download="<?php echo $row['title']; ?>">
-                                                                    <button type="button" class="btn col-12" data-toggle="modal" ><i class="fad fa-cloud-download-alt"></i> Download</button>
-                                                                </a>
-                                                                <div class="dropdown-divider"></div>
-                                                                <a class="dropdown-item" href="#"><button type="button" class="btn col-12"><i class="fad fa-trash"></i> Delete</button></a>
-                                                            </div>
-                                                        </div>    
-                                                    </div>        
-                                                </div>
-                                                <a href="<?php $image_user_id=$row['user_id'];
-                                                $sql="SELECT * FROM users WHERE id='$image_user_id'";
-                                                $result_img_r=$connect->query($sql);
-                                                $row_img_user=$result_img_r->fetch_assoc();
-                                                $username=$row_img_user['username'];
-                                                $fullname=$row_img_user['name'];
-                                                echo $site_url.'/pages/profile.php?u='.$username; ?>" class=" text-decoration-none text-white"><img class="upimg" src="https://picsum.photos/id/237/200/300" alt=""> <?php echo $fullname; ?></a>
-                                                <div class="container">
-                                                    <div class="row chbtn">
-                                                        <?php
-                                                            $image_id=$row['id'];
-                                                                if($login==1){
-                                                                    //Check liked or not
-                                                                    $sql="SELECT * FROM likes WHERE image_id='$image_id' AND user_id='$user_id'";
-                                                                    $result_like=$connect->query($sql);
-                                                                    if($result_like->num_rows==1){
-                                                                        $icon="fad";
-                                                                        $like_color="color:red;";
-                                                                    }
-                                                                    else{
-                                                                        $icon="fal";
-                                                                        $like_color="";
-                                                                    }
-                                                                }
-                                                                else{
-                                                                    $icon="fal";
-                                                                    $like_color="";
-                                                                }
-                                                        ?>
-                                                        <a class="btn btn-outline-danger cbtn" style="margin-right: 5px;" id="<?php echo $image_id; ?>" onclick="mylike(<?php echo $image_id; ?>)" title="Like This Image"><span style="<?php echo $like_color;?>"><i class="<?php echo $icon; ?> fa-heart"></i></span> <span><?php echo $row['likes']; ?></span></a>
-                                                        <a href="<?php echo $site_url; ?>/pages/image.php?id=<?php echo $row['image_id']; ?>" class="btn btn btn-outline-light cbtn" title="View Image" style="margin-left: 5px;"><i class="fad fa-eye"></i> <span><?php echo $row['views']; ?></span></a>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                <?php endwhile;}
-                                else{
-                                    echo "<center><b>No Image Found!</b></center>";
-                                    } ?>
-                                <!--user uploaded image end-->
-            </div>
-        </div>
-        <div class="text-center"><button type="button" class="btn btn-info mt-4 mb-2" style="border-radius: 1.25rem;"><i class="fad fa-plus-circle"></i> Load More</button></div>
+        <div id="loadData" class="col-md-12 pb-3 pt-2"><h1 class="text-center chdr" style="color:crimson"><i class="fad fa-fire"></i> Trendings</h1></div>
+        <!-- <div class="container shadow-lg p-3" style="border-radius: 1.25rem;"> -->
+                        <!-- Load Images Here -->
+                        <!-- Load Button Here -->
+        <!-- </div> -->
     </div>
     <!----------------------Footer Section---------------------------------------------------->
     <?php require_once('include/footer.php'); ?>
@@ -138,5 +64,37 @@
             }
         </script>
         <?php } ?>
+
+<script>
+  $(document).ready(function(){
+    // Load Data from Database with Ajax
+    function loadTable(page){
+      $.ajax({
+        url: "pagination.php",
+        type: "POST",
+        data : { page_no : page },
+        success: function(data){
+          if(data){
+            $("#pagination").remove();
+            $("#loadData").append(data);
+          }else{
+            $("#ajaxbtn").html("Finished!");
+            $("#ajaxbtn").prop("disabled",true);
+          }
+          
+        }
+      });
+    }
+    loadTable();
+
+    // Add Click Event on ajaxbtn
+    $(document).on("click","#ajaxbtn",function(){
+      $("#ajaxbtn").html("Loading...");
+      var pid = $(this).data("id");
+      loadTable(pid);
+    });
+
+  });
+</script>
 </body>
 </html>
