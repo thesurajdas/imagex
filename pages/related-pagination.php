40392e3ab24e3df46aaca5f704a6752d54d510cc
Suraj Das <blogwapz@gmail.com>
2021-06-14 15:15:01 +0530

    fix image related pagination

--- pages/related-pagination.php
+++ pages/related-pagination.php
@@ -21,7 +21,7 @@ else{
 }
 $last_id = $page+$limit;
 	                            //Get Image Data from Database
-	                                                    $sql="SELECT * FROM images WHERE category={$category_id} LIMIT {$page},$limit";
+	                                                    $sql="SELECT * FROM images WHERE category={$category_id} ORDER BY likes DESC, views DESC, downloads DESC LIMIT {$page},$limit";
                                                         $result_img=$connect->query($sql);
                                                         if ($result_img->num_rows>0) {
                                                         while($row=$result_img->fetch_assoc()):
