bcbf1e77d3666ac131a556b4208f834e7bd04383
Suraj Das <blogwapz@gmail.com>
2021-06-18 11:24:42 +0530

    fix image visibility in trending and category

--- pages/category-pagination.php
+++ pages/category-pagination.php
@@ -16,7 +16,7 @@ else{
 }
 $last_id = $page+$limit;
 	                            //Get Image Data from Database
-	                            $sql="SELECT * FROM images WHERE category={$id} ORDER BY time DESC LIMIT {$page},{$limit}";
+	                            $sql="SELECT * FROM images WHERE visibility=0 AND category={$id} ORDER BY time DESC LIMIT {$page},{$limit}";
 	                            $result_img=$connect->query($sql);
 	                            if ($result_img->num_rows>0) {
                                 while($row=$result_img->fetch_assoc()):?>
