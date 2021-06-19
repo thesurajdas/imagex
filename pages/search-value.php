2ea3cf057048bae855e1064f73b0d59228a72e55
Suraj Das <blogwapz@gmail.com>
2021-06-19 16:03:21 +0530

    improve search experience

--- pages/search-value.php
+++ pages/search-value.php
@@ -3,7 +3,8 @@
     if (isset($_GET['q'])) {
         $query=$_GET['q'];
         $output="";
-        $sql="SELECT DISTINCT(title) FROM images WHERE visibility=0 AND title LIKE '%{$query}%' LIMIT 5";
+        $output.='<a href="'.$site_url.'/pages/search.php?q='.strtolower($query).'" class="dropdown-item" "><i class="fad fa-search"></i> '.strtolower($query)."</a>";
+        $sql="SELECT DISTINCT(title) FROM images WHERE visibility=0 AND title LIKE '%{$query}%' LIMIT 7";
         $result=$connect->query($sql);
         $output.='';
         if ($result->num_rows>0) {
@@ -14,7 +15,7 @@
 else{
     $output.="<p class='dropdown-item''>No Data Found!</p>";
 }
-$output.='<div class="dropdown-divider"></div><a href="'.$site_url.'/pages/search.php?q='.strtolower($query).'" class="dropdown-item"><i class="fad fa-plus"></i> Show More <i class="fad fa-angle-double-right"></i></a>';
+$output.='<div class="dropdown-divider"></div><a href="'.$site_url.'/pages/search.php?q=" class="dropdown-item"><i class="fad fa-plus"></i> Show all <i class="fad fa-angle-double-right"></i></a>';
 echo $output;
 }
 ?>
