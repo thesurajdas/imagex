893d66c7bcb36224aabaf0b8d7116924e6a5bfb5
Suraj Das <blogwapz@gmail.com>
2021-06-19 15:56:02 +0530

    Add show more button

--- pages/search-value.php
+++ pages/search-value.php
@@ -9,12 +9,12 @@
         if ($result->num_rows>0) {
             while ($row=$result->fetch_assoc()) {
         $output.='<a href="'.$site_url.'/pages/search.php?q='.strtolower($row['title']).'" class="dropdown-item" "><i class="fad fa-search" style="color: #3654a9"></i> '.strtolower($row['title'])."</a>";
-            }
-        }
-        else{
-            $output.="<p class='dropdown-item''>No Data Found!</p>";
-        }
-        $output.="";
-        echo $output;
     }
+}
+else{
+    $output.="<p class='dropdown-item''>No Data Found!</p>";
+}
+$output.='<div class="dropdown-divider"></div><a href="'.$site_url.'/pages/search.php?q='.strtolower($query).'" class="dropdown-item"><i class="fad fa-plus"></i> Show More <i class="fad fa-angle-double-right"></i></a>';
+echo $output;
+}
 ?>
