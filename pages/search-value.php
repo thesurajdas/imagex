0b9c9ba47075035f40403c70640853359e06e3bd
Suraj Das <blogwapz@gmail.com>
2021-06-19 21:27:25 +0530

    fix search experience

--- pages/search-value.php
+++ pages/search-value.php
@@ -10,12 +10,9 @@
         if ($result->num_rows>0) {
             while ($row=$result->fetch_assoc()) {
         $output.='<a href="'.$site_url.'/pages/search.php?q='.strtolower($row['title']).'" class="dropdown-item" "><i class="fad fa-search" style="color: #3654a9; font-size: 13px"></i> '.strtolower($row['title'])."</a>";
+        $output.='<div class="dropdown-divider"></div><a href='.$site_url.'/pages/search.php?q=" class="dropdown-item" style="color: green; font-weight: 600;"><i class="fad fa-search-plus"></i> More Results</a>';
     }
 }
-else{
-    $output.="<p class='dropdown-item''><i class='fad fa-sad-tear'></i> No Results Found!</p>";
-}
-$output.='<div class="dropdown-divider"></div><a href='.$site_url.'/pages/search.php?q=" class="dropdown-item" style="color: green; font-weight: 600;"><i class="fad fa-search-plus"></i> More Results</a>';
 echo $output;
 }
 ?>
