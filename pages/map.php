bd5384e96d2541df4bde1a200bb7c701dda37613
Akash Sarkar <akash.sarkar1489@outlook.com>
2021-07-02 19:05:46 +0530

    add comment

--- pages/map.php
+++ pages/map.php
@@ -27,7 +27,22 @@
         <div class="container shadow-lg p-3 mb-5 bg-white glry" style="border-radius: 1.25rem;">
             <div  class="col-md-12 pb-3 pt-2"><h1 class="text-center" style="color: rgb(23 109 222 / 72%);"><i class="fad fa-globe-asia"></i> Images From The Globe</h1></div>
             <div class="container bg-light shadow-lg p-3" style="border-radius: 1.25rem;">
+                <div class="row pl-3 pr-3 pt-3">
+                    <div class="text-right col-12">
+                        <div class="btn-group btn-group-sm" role="group">
+                            <button id="btnGroupDrop1" type="button" class="btn btn-secondary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="border-radius: 1.25rem;"><i class="fad fa-caret-circle-down"></i> Short By</button>
+                            <div class="dropdown-menu" style="min-width: auto;" aria-labelledby="btnGroupDrop1">
+                                <a class="dropdown-item " style="text-align: start; color: rgba(99, 96, 96, 0.856); font-size: 80%; font-weight: 700;" href="?id=<?php echo $cat_id; ?>&s=views">View Item</a>
+                                <a class="dropdown-item " style="text-align: start; color: rgba(220, 20, 60, 0.842); font-size: 80%; font-weight: 700;" href="?id=<?php echo $cat_id; ?>&s=likes">View Item</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- -------------map start------------------- -->
 
+
+                
+                <!-- -------------map end------------------- -->
             </div>
         </div>
     <?php require_once('include/footer.php'); ?>    
