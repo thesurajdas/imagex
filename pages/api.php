5eda73c7dd51856589b39b92d06ed5600c5ce327
Suraj Das <blogwapz@gmail.com>
2021-07-02 12:00:01 +0530

    improve api performance

--- pages/api.php
+++ pages/api.php
@@ -1,24 +1,27 @@
 <?php
-    // header('Content-Type: image/jpeg');
-    // if(stripos($_GET['image'],'.png')){
-    //     $img=imagecreatefrompng($_GET['image']);
-    // }
-    // elseif((stripos($_GET['image'],'.jpg')) or (stripos($_GET['image'],'.jpeg'))){
-    //     $img=imagecreatefromjpeg($_GET['image']);
-    // }
-    // else{
-    //     $img=imagecreatefromgif($_GET['image']);
-    // }
-    // $imageresize=imagescale($img,$_GET['w'],$_GET['h'],IMG_BICUBIC_FIXED);
-    // imagejpeg($imageresize);
-    // imagedestroy($img);
-    // imagedestroy($imageresize);
-
     header('Content-Type: image/jpeg');
     $file=$_GET['image'];
     list($width,$height)=getimagesize($file);
-    $nwidth=$width/5;
-    $nhieght=$height/5;
+    if(($width>=4000) OR ($height>=4000)){
+        $nwidth=$width/10;
+        $nhieght=$height/10;
+    }
+    elseif(($width>=3000) OR ($height>=3000)){
+        $nwidth=$width/8;
+        $nhieght=$height/8;
+    }
+    elseif(($width>=2000) OR ($height>=2000)){
+        $nwidth=$width/6;
+        $nhieght=$height/6;
+    }
+    elseif(($width>=1000) OR ($height>=1000)){
+        $nwidth=$width/4;
+        $nhieght=$height/4;
+    }
+    else{
+        $nwidth=$width/2;
+        $nhieght=$height/2;
+    }
     $newimage=imagecreatetruecolor($nwidth,$nhieght);
     $source=imagecreatefromjpeg($file);
     imagecopyresized($newimage,$source,0,0,0,0,$nwidth,$nhieght,$width,$height);
