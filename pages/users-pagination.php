c3c14fa35d89d488e5586665d293f0a8a943be97
Suraj Das <blogwapz@gmail.com>
2021-07-08 14:18:56 +0530

    fix users page

--- pages/users-pagination.php
+++ pages/users-pagination.php
@@ -10,31 +10,31 @@ if(isset($_POST['page_no'])){
 }
 $last_id = $page+$limit;
 	                            //Get Image Data from Database
-	                            $sql="SELECT * FROM users LIMIT {$page},$limit";
+	                            $sql="SELECT * FROM users ORDER BY total_views+total_likes DESC LIMIT {$page},$limit";
 	                            $result_user=$connect->query($sql);
 	                            if ($result_user->num_rows>0) {
                                 while($ru=$result_user->fetch_assoc()):
-                            // $tmp_id=$ru['id'];
-                            // //Temp variables
-                            // $user_total_views=0;
-                            // $user_total_likes=0;
-                            // //Get users uploaded images data
-                            // $sql2="SELECT * FROM images WHERE user_id='$tmp_id'";
-                            // $result_user=$connect->query($sql2);
-                            // if ($result_user->num_rows>0) {
-                            //     //total views
-                            //     while($row_img=$result_user->fetch_assoc()){
-                            //         $user_total_views=$user_total_views+$row_img['views'];
-                            //         $user_total_likes=$user_total_likes+$row_img['likes'];
-                            //     }
-                            // }
+                                  $u_id=$ru['id'];
+                                  $user_total_views=0;
+            $user_total_likes=0;
+            //Get users uploaded images data
+            $sql="SELECT * FROM images WHERE user_id='$u_id'";
+            $result_img=$connect->query($sql);
+            //total views
+            while($row_img=$result_img->fetch_assoc()){
+                $user_total_views=$user_total_views+$row_img['views'];
+                $user_total_likes=$user_total_likes+$row_img['likes'];
+            }
+            //Update total views and likes in user
+            $sql="UPDATE users SET total_views = '$user_total_views', total_likes = $user_total_likes WHERE id='$u_id';";
+            $connect->query($sql);
                     ?>
                     <div class="col-md-4 col-xl-4 col-lg-4 col-sm-12 mb-5">
                         <div class="bg-white shadow-sm py-5 px-4" style="border-radius: 1.25rem;"><img src="../<?php if(!empty($ru['avatar'])){echo $ru['avatar']; } else {echo 'img/avatar.png';}  ?>" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm cthhumb">
                             <h5 class="mb-0 "><a class="text-decoration-none" href="profile.php?u=<?php echo $ru['username']; ?>" target="_blank"><?php echo $ru['name']; ?></a></h5><span class="small text-uppercase text-muted"><?php echo $ru['role']; ?></span>
                             <ul class="social mb-0 list-inline mt-3 mb-2">
-                                <li class="list-inline-item"><a href="#" class="btn fbb" style="border-radius: 1.25rem;" disabled><i class="fad fa-eye" style="color: #212529bf;"></i> <span><?php //echo  $user_total_views; ?></span></a></li>
-                                <li class="list-inline-item"><a href="#" class="btn tww"  style="border-radius: 1.25rem;" disabled><i class="fad fa-heart" style="color: #ff0000c2;"></i> <span><?php //echo  $user_total_likes; ?></span></a></li>
+                                <li class="list-inline-item"><a href="#" class="btn fbb" style="border-radius: 1.25rem;" disabled><i class="fad fa-eye" style="color: #212529bf;"></i> <span><?php echo $user_total_views; ?></span></a></li>
+                                <li class="list-inline-item"><a href="#" class="btn tww"  style="border-radius: 1.25rem;" disabled><i class="fad fa-heart" style="color: #ff0000c2;"></i> <span><?php echo $user_total_likes; ?></span></a></li>
                             </ul>
                             <!-- <button type="button" class="btn btn-outline-warning" style="border-radius: 1.25rem;"><i class="fad fa-user-plus"></i> Follow</button> -->
                         </div>
