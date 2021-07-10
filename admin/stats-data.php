c421b661b45fd1dbd6950b337bd1bcc30fc439cd
Suraj Das <blogwapz@gmail.com>
2021-07-10 22:20:15 +0530

    fix some issue with year

--- admin/stats-data.php
+++ admin/stats-data.php
@@ -1,16 +1,16 @@
 <?php
     require('../connect.php');
+    $year = date("Y");
     if((isset($_GET['id'])) && ($_GET['id']=='i_chart')){        
         for($i=0;$i<=12;$i++){
             $num = $i;
             $n = sprintf("%02d", $num);
             // echo $n;
-            $sql="SELECT * FROM images WHERE time LIKE '2021-$n%'";
+            $sql="SELECT * FROM images WHERE time LIKE '$year-$n%'";
             $result=$connect->query($sql);
             $data = array();
                 if((!empty($result->num_rows)) && ($result->num_rows!=0)){
                     $data[] = $result->num_rows;
-                    $result->close();
                     foreach($data as $value){
                         echo $value . ",";
                     }
@@ -22,12 +22,11 @@
             $num = $i;
             $n = sprintf("%02d", $num);
             // echo $n;
-            $sql="SELECT * FROM users WHERE register_date LIKE '2021-$n%'";
+            $sql="SELECT * FROM users WHERE register_date LIKE '$year-$n%'";
             $result=$connect->query($sql);
             $data = array();
                 if((!empty($result->num_rows)) && ($result->num_rows!=0)){
                     $data[] = $result->num_rows;
-                    $result->close();
                     foreach($data as $value){
                         echo $value . ",";
                     }
