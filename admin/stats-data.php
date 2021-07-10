f6048b24cdeb324c0913ddfc66ec213f03ac248d
Suraj Das <blogwapz@gmail.com>
2021-07-10 22:17:47 +0530

    add live stats

--- admin/stats-data.php
+++ admin/stats-data.php
@@ -1,29 +1,54 @@
 <?php
     require('../connect.php');
     if((isset($_GET['id'])) && ($_GET['id']=='i_chart')){        
-        $sql="SELECT * FROM i_chart;";
-        $result=$connect->query($sql);
-        $data = array();
-        foreach ($result as $row) {
-            $data[] = $row['i_data'];
-        }
-        $result->close();
-        // print json_encode($data);
-        foreach($data as $value){
-            echo $value . ",";
+        for($i=0;$i<=12;$i++){
+            $num = $i;
+            $n = sprintf("%02d", $num);
+            // echo $n;
+            $sql="SELECT * FROM images WHERE time LIKE '2021-$n%'";
+            $result=$connect->query($sql);
+            $data = array();
+                if((!empty($result->num_rows)) && ($result->num_rows!=0)){
+                    $data[] = $result->num_rows;
+                    $result->close();
+                    foreach($data as $value){
+                        echo $value . ",";
+                    }
+                }
         }
     }
-    if((isset($_GET['id'])) && ($_GET['id']=='u_chart')){        
-        $sql="SELECT * FROM u_chart;";
-        $result=$connect->query($sql);
-        $data = array();
-        foreach ($result as $row) {
-            $data[] = $row['u_data'];
+    elseif((isset($_GET['id'])) && ($_GET['id']=='u_chart')){        
+        for($i=0;$i<=12;$i++){
+            $num = $i;
+            $n = sprintf("%02d", $num);
+            // echo $n;
+            $sql="SELECT * FROM users WHERE register_date LIKE '2021-$n%'";
+            $result=$connect->query($sql);
+            $data = array();
+                if((!empty($result->num_rows)) && ($result->num_rows!=0)){
+                    $data[] = $result->num_rows;
+                    $result->close();
+                    foreach($data as $value){
+                        echo $value . ",";
+                    }
+                }
         }
-        $result->close();
-        // print json_encode($data);
-        foreach($data as $value){
-            echo $value . ",";
+    }
+    else{
+        for($i=0;$i<=12;$i++){
+            $num = $i;
+            $n = sprintf("%02d", $num);
+            // echo $n;
+            $sql="SELECT * FROM users WHERE register_date LIKE '2021-$n%'";
+            $result=$connect->query($sql);
+            $data = array();
+                if((!empty($result->num_rows)) && ($result->num_rows!=0)){
+                    $data[] = $result->num_rows;
+                    $result->close();
+                    foreach($data as $value){
+                        echo $value . ",";
+                    }
+                }
         }
     }
 ?>
